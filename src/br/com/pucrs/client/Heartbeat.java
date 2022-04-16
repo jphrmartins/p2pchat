80616bf10a4017a7e325ff465bc6dc6f39baba0b
Julia <juliamakowski@hotmail.com>
2022-04-16 18:42:21 -0300

    WIP - comeco do heartbeat no cliente

--- src/br/com/pucrs/client/Heartbeat.java
+++ src/br/com/pucrs/client/Heartbeat.java
@@ -1,2 +1,34 @@
-package br.com.pucrs.client;public class Heartbeat {
+import br.com.pucrs.remote.api.RemoteServerApi;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Heartbeat extends Thread {
+    protected String addr = null;
+    protected byte[] data = new byte[1024];
+    protected int port;
+    private RemoteServerApi remoteServerApi;
+
+    public Heartbeat(RemoteServerApi remoteServerApi, String addr, int port) throws UnknownHostException {
+        this.remoteServerApi = remoteServerApi;
+        data = ("heartbeat " + InetAddress.getLocalHost().getHostAddress()).getBytes();
+        this.addr = addr;
+        this.port = port;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+
+            } catch (Exception e) {
+            }
+
+            try {
+                Thread.sleep(3000);
+            } catch(InterruptedException e) {
+            }
+//			System.out.println("\npulse!");
+        }
+    }
 }
